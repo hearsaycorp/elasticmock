73eb0908d06c6a08193a8a622b73a24e3bc28bd3
gcseter <gcseter@hearsaycorp.com>
2024-10-09 10:06:45 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,10 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         "License :: OSI Approved :: MIT License",
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
